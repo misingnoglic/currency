a7cfe9f9c77ab71df2cfc9357947eaca69641377
Arya Boudaie <arya@aryaboudaie.com>
2026-03-26 13:56:37 +0700

    Autoupdate

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
+
+// Check for updates immediately, and then every hour
+registerSW({
+  onRegistered(r: any) {
+    r && setInterval(() => {
+      r.update()
+    }, 60 * 60 * 1000)
+  }
+})
+
+// When the new service worker takes over, reload the page immediately so the user doesn't see a cached version
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.location.reload()
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
